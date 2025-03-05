966696e29c210364e3cb20b52c41084b50a85f96
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-03-05 13:43:31 -0800

    update states,fetch API data

---
+++ src/components/recipe.jsx
@@ -0,0 +1,24 @@
+import React, {useState,useEffect} from react;
+const RecipeList = () => {
+  const [recipes, setRecipes] = useState([]);//State to store the filtered recipes
+  const [ingregient, setIngregient]= useState('chicken_breast');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
+useEffect(async () => {
+  try{
+    setLoading(true); // Start loading
+    setError(null); // Reset error before fetching
+    
+    // Making the API call
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+
+
+    setLoading(false); // Stop loading
+  }
+  catch(error){
+    setError("Failed to fetch recipes.");
+    setLoading(false); // Stop loading even if an error occurs
+  }
+});
+export default RecipeList;
