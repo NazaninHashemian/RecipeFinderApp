2e9aad11e0f8bfc6d2ad19d537cfbdcc7ca4f002
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-03-05 14:16:11 -0800

    Update readme file and correction of RecipeList component structure

--- src/components/recipeList.jsx
+++ src/components/recipeList.jsx
@@ -1,29 +1,37 @@
-import React, {useState,useEffect} from react;
+// src/components/RecipeList.jsx
+import React, { useState, useEffect } from 'react';
 const RecipeList = () => {
-  const [recipes, setRecipes] = useState([]);//State to store the filtered recipes
-  const [ingregient, setIngregient]= useState('chicken_breast');
+  const [recipes, setRecipes] = useState([]); //State to store the filtered recipes
+  const [ingredient, setIngredient] = useState('chicken_breast');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true); // Start loading
+        setError(null); // Reset error before fetching
+
+        // Making the API call
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+        );
+
+        // If response is successful, parse the data as JSON
+        const data = await response.json();
+        console.log(data);
+
+        if (data.meals) {
+        }
+        setLoading(false); // Stop loading
+      } catch (error) {
+        setError('Failed to fetch recipes.');
+        setLoading(false); // Stop loading even if an error occurs
+      }
+    };
+  }, [ingredient]);
+
+  return <></>;
 };
-useEffect(async () => {
-  try{
-    setLoading(true); // Start loading
-    setError(null); // Reset error before fetching
 
-    // Making the API call
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-
-    // If response is successful, parse the data as JSON
-    const data = await response.json();
-    console.log(data);
-
-
-
-    setLoading(false); // Stop loading
-  }
-  catch(error){
-    setError("Failed to fetch recipes.");
-    setLoading(false); // Stop loading even if an error occurs
-  }
-});
 export default RecipeList;
