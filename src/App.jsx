f52e111140fae80dbf67fd126abcde8a62ff5ed6
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-03-10 13:32:14 -0700

    Add conditional rendering in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 //App.jsx
+import { useState } from 'react';
 import RecipeList from './components/RecipeList';
 import MealName from './components/MealName';
 import './app.css';
@@ -6,10 +7,17 @@ import './app.css';
 function App() {
   const [activeComponent, setActiveComponent] = useState('mealName');
   return (
-    <>
-      <MealName />
-      <RecipeList />
-    </>
+    <div>
+      <div>
+        <button onClick={() => setActiveComponent('mealName')}>
+          Meal Name
+        </button>
+        <button onClick={() => setActiveComponent('mainIngredien')}>
+          Recipe List
+        </button>
+      </div>
+      {activeComponent == 'mealName' ? <MealName /> : <RecipeList />}
+    </div>
   );
 }
 
