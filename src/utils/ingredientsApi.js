7724320013fa30d113f59e03f1d4db8a5b75e870
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-03-13 00:40:05 -0700

    Create ingredientsApi.js

---
+++ src/utils/ingredientsApi.js
@@ -0,0 +1,17 @@
+//src/utils/ingredientsApi.js
+const fetchIngredients = async () => {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/list.php?i=list`
+    );
+    const data = await response.json();
+    if (data.meals) {
+      return data.meals.map((meal) => meal.strIngredient);
+    }
+    return []; // If no ingredients are found, return an empty array
+  } catch (error) {
+    console.error('Error fetching cuisines:', error);
+    return []; // Return an empty array in case of an error
+  }
+};
+export default fetchIngredients;
