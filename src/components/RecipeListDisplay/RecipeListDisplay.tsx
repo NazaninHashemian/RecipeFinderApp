a7dda25f05257b3c53bc74283dfd496b8bb6f19d
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-05-20 15:31:23 -0700

    Create RecipeListDisplay.css

--- src/components/RecipeListDisplay/RecipeListDisplay.tsx
+++ src/components/RecipeListDisplay/RecipeListDisplay.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RecipeCard from '../RecipeCard/RecipeCard';
 import Recipe from '../../utils/types';
+import './RecipeListDisplay.css';
 
 interface RecipeListDisplayProps {
   recipes: Recipe[];
