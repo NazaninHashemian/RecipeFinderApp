1502738235722b9d177b23eaa4698f211f4e1b78
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-05-21 09:37:26 -0700

    Create Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { GiCook } from "react-icons/gi";
+import { NavLink } from "react-router-dom"
+
+interface NavbarProps {
+    isDarkMode: boolean;
+}
+
+const activeHandling = ({ isActive}:{ isActive: boolean}): string => 
+    (isActive ? 'active' : ''); 
+
+const Navbar: React.FC<NavbarProps> = ({ isDarkMode }) => {
+    
+    return (
+        <div className="nav">
+        <GiCook 
+          size={38} 
+          color={isDarkMode ? '#fff' : '#ff6347'} 
+          style={{ marginRight: 10, marginTop: 5 }} 
+          className="hide-on-small"
+        />
+        <NavLink to="/name" className={activeHandling}>
+          <button>Name</button>
+        </NavLink>
+
+        <NavLink to="/first-letter" className={activeHandling}>
+          <button>First Letter</button>
+        </NavLink>
+
+        <NavLink to="/ingredient" className={activeHandling}>
+          <button>Ingredient</button>
+        </NavLink>
+
+        <NavLink to="/cuisine" className={activeHandling}>
+          <button>Cuisine</button>
+        </NavLink>
+
+        <NavLink to="/random" className={activeHandling}>
+          <button>Random</button>
+        </NavLink>
+
+        <NavLink to="/categories" className={activeHandling}>
+          <button>Categories</button>
+        </NavLink>
+        
+      </div>
+    )
+}
+
+export default Navbar;
