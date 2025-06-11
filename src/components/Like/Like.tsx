69a35191d616b721add1242d2e80347bed2eddaf
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-06-11 11:25:13 -0700

    Create Like component

---
+++ src/components/Like/Like.tsx
@@ -0,0 +1,18 @@
+// Like.tsx
+import React from 'react';
+import { IoIosHeart, IoIosHeartEmpty } from 'react-icons/io';
+
+interface Props {
+  onClick: () => void;
+  liked: boolean;
+}
+
+const Like: React.FC<Props> = ({ onClick, liked }) => {
+  return liked ? (
+    <IoIosHeart color="#ff6b81" size={20} onClick={onClick} />
+  ) : (
+    <IoIosHeartEmpty size={20} onClick={onClick} />
+  );
+};
+
+export default Like;  // <-- Make sure to export it!
